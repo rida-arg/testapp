3b670113299808e159447601b9d80733744c7170
argane <rida.argane@gmail.com>
2024-01-04 10:57:32 +0100

    first commit

--- backend_for_my_app/controllers/auth.js
+++ backend_for_my_app/controllers/auth.js
@@ -1,5 +1,5 @@
 const db = require('../models');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Joi = require('joi');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
